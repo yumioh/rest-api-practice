9bf6e2017824e095e3f19b0bbaf41abfad7e1fc9
yumioh <ymoh@korda.im>
2023-02-19 18:08:55 +0900

    /api/event

--- src/main/java/com/example/demo/events/EventController.java
+++ src/main/java/com/example/demo/events/EventController.java
@@ -1,5 +1,6 @@
 package com.example.demo.events;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,10 +15,16 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 @Controller
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
+
+    private final EventRepository eventRepository;
+    public EventController(EventRepository eventRepository){
+        this.eventRepository= eventRepository;
+    }
     @PostMapping
    public ResponseEntity createEvent(@RequestBody Event event){
-        URI createUri = linkTo(EventController.class).slash("{id}").toUri();
+        Event newEvent = this.eventRepository.save(event);
+        URI createUri = linkTo(EventController.class).slash( newEvent.getId()).toUri();
         event.setId(10);
-        return ResponseEntity.created(createUri).build(event);
+        return ResponseEntity.created(createUri).body(event);
    }
 }
--- src/test/java/com/example/demo/events/EventControllerTests.java
+++ src/test/java/com/example/demo/events/EventControllerTests.java
@@ -2,9 +2,14 @@ package com.example.demo.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -14,6 +19,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class EventControllerTests {
 
     @Autowired
@@ -21,6 +28,9 @@ public class EventControllerTests {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
@@ -36,9 +46,11 @@ public class EventControllerTests {
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타텁 팩토리")
                 .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events/")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
                         .accept(MediaTypes.HAL_JSON)
                         .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
