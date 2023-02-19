51fece790fa7eda4fe36596bc113e330056b20ea
yumioh <iove0103@naver.com>
2023-02-19 20:22:24 +0900

    add EventDto

---
+++ src/main/java/com/example/demo/events/EventDto.java
@@ -0,0 +1,28 @@
+package com.example.demo.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.time.LocalDateTime;
+
+@Builder @NoArgsConstructor @AllArgsConstructor @Data
+public class EventDto {
+    private Integer id; //식별자
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location; // (optional) 이게 없으면 온라인 모임
+    private int basePrice; // (optional)
+    private int maxPrice; // (optional)
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+}
