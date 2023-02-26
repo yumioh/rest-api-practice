3e3acbb74171145e853d3a30ad2daa492dd25876
yumioh <iove0103@naver.com>
2023-02-26 13:24:54 +0900

    "badrequest response"

--- src/test/java/com/example/demo/events/EventTest.java
+++ src/test/java/com/example/demo/events/EventTest.java
@@ -1,9 +1,13 @@
 package com.example.demo.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
     @Test
@@ -33,4 +37,47 @@ public class EventTest {
         //assert는 개발/테스트 단계에서 파라미터가 제대로 넘어 왔는지 /NULL이 들어오면 작동 안됨
     }
 
+    @Test
+    @Parameters//(method = "parameterForTestFree")
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
+        //Given
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+        //when
+        event.update();
+        //then
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
+
+    private Object[] parametersForTestFree() {
+        return new Object[] {
+                new Object[] {0,0,true},
+                new Object[] {100,0,false},
+                new Object[] {0,100,false},
+                new Object[] {100,200,false},
+        };
+    }
+
+    @Test
+    @Parameters
+    public void testOffline(String location, boolean isOffline){
+        //Given
+        Event event = Event.builder()
+                .location(location)
+                .build();
+        //when
+        event.update();
+        //then
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
+
+    private Object[] parametersForTestOffline() {
+        return new Object[] {
+                new Object[] {"강남",true},
+                new Object[] {null,false},
+                new Object[] {"      ",false}
+        };
+    }
 }
